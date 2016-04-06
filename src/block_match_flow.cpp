501548d16ac012540a9d4a330e98c371d1e68374
Dan Butler <djbutler@cs.washington.edu>
2016-04-06 02:06:21 -0700

    use Halide namespace

--- src/block_match_flow.cpp
+++ src/block_match_flow.cpp
@@ -8,6 +8,7 @@
 
 // The only Halide header file you need is Halide.h. It includes all of Halide.
 #include "Halide.h"
+using namespace Halide;
 
 // Include some support code for loading pngs.
 #include "halide_image_io.h"
@@ -19,40 +20,40 @@ int main(int argc, char **argv) {
     const int HALF_BLOCK_WIDTH = 3;
     const int HALF_DISPLACEMENT = 10;
     
-    Halide::Target target = Halide::get_target_from_environment();
-    bool has_opengl = target.has_feature(Halide::Target::OpenGLCompute);
+    Target target = get_target_from_environment();
+    bool has_opengl = target.has_feature(Target::OpenGLCompute);
     printf("has_opengl = %d\n", has_opengl);
 
     // First we'll load the image pair 
-    Halide::Image<uint8_t> input0 = load_image("../data/slomo/0.png");
-    Halide::Image<uint8_t> input1 = load_image("../data/slomo/1.png");
-    Halide::Func padded0 = Halide::BoundaryConditions::repeat_edge(input0);
-    Halide::Func padded1 = Halide::BoundaryConditions::repeat_edge(input1);
+    Image<uint8_t> input0 = load_image("../data/slomo/0.png");
+    Image<uint8_t> input1 = load_image("../data/slomo/1.png");
+    Func padded0 = BoundaryConditions::repeat_edge(input0);
+    Func padded1 = BoundaryConditions::repeat_edge(input1);
 
-    Halide::Func conv;
-    Halide::RDom block(-HALF_BLOCK_WIDTH, 2*HALF_BLOCK_WIDTH+1, -HALF_BLOCK_WIDTH, 2*HALF_BLOCK_WIDTH+1);
-    Halide::Var x, y, i, j;
+    Func conv;
+    RDom block(-HALF_BLOCK_WIDTH, 2*HALF_BLOCK_WIDTH+1, -HALF_BLOCK_WIDTH, 2*HALF_BLOCK_WIDTH+1);
+    Var x, y, i, j;
 
     // NOTE: only uses one color channel (for speed)
-    Halide::Expr diff =
-        Halide::cast<float>(padded0(x+block.x, y+block.y, 0)) -
-        Halide::cast<float>(padded1(x+i+block.x, y+j+block.y, 0));
+    Expr diff =
+        cast<float>(padded0(x+block.x, y+block.y, 0)) -
+        cast<float>(padded1(x+i+block.x, y+j+block.y, 0));
 
     conv(x, y, i, j) = 
-        Halide::sum(diff * diff);
-        //Halide::sum(Halide::cast<int32_t>(diff * diff));
+        sum(diff * diff);
+        //sum(cast<int32_t>(diff * diff));
 
     const int SCALE_FACTOR = 10;
-    Halide::Func flow;
-    Halide::RDom displacement(-HALF_DISPLACEMENT, 2*HALF_DISPLACEMENT+1, -HALF_DISPLACEMENT, 2*HALF_DISPLACEMENT+1);
-    Halide::Tuple disp_tup = Halide::argmin(conv(x, y, displacement.x, displacement.y));
-    flow(x, y) = Halide::Tuple(Halide::cast<uint8_t>(disp_tup[0] * SCALE_FACTOR + 127),
-                               Halide::cast<uint8_t>(disp_tup[1] * SCALE_FACTOR + 127),
+    Func flow;
+    RDom displacement(-HALF_DISPLACEMENT, 2*HALF_DISPLACEMENT+1, -HALF_DISPLACEMENT, 2*HALF_DISPLACEMENT+1);
+    Tuple disp_tup = argmin(conv(x, y, displacement.x, displacement.y));
+    flow(x, y) = Tuple(cast<uint8_t>(disp_tup[0] * SCALE_FACTOR + 127),
+                               cast<uint8_t>(disp_tup[1] * SCALE_FACTOR + 127),
                                disp_tup[2]);
                   
     // Tiles of size 128x128, xy-fused, and parallelized 
-    Halide::Var xo, yo, xi, yi;
-    Halide::Var tile_index;
+    Var xo, yo, xi, yi;
+    Var tile_index;
     flow
         .tile(x, y, xo, yo, xi, yi, 128, 128)
         .fuse(xo, yo, tile_index)
@@ -66,18 +67,18 @@ int main(int argc, char **argv) {
     // Also vectorize the producer (because sin is vectorizable on x86 using SSE).
     conv.vectorize(i, 4);
 
-    Halide::Realization r = 
+    Realization r = 
         flow.realize(input0.width(), input0.height()); 
 
     printf("Realization r = %zu\n", r.size());
 
-    Halide::Image<uint8_t> flow_x = r[0];
+    Image<uint8_t> flow_x = r[0];
     save_image(flow_x, "flow_x.png");
 
-    Halide::Image<uint8_t> flow_y = r[1];
+    Image<uint8_t> flow_y = r[1];
     save_image(flow_y, "flow_y.png");
 
-    Halide::Image<float> cost = r[2];
+    Image<float> cost = r[2];
     save_image(cost, "cost.png");
 
     printf("Pseudo-code for the conv schedule:\n");
@@ -88,7 +89,7 @@ int main(int argc, char **argv) {
     flow.print_loop_nest();
     printf("\n");
 
-    flow.compile_to_lowered_stmt("flow.html", {}, Halide::HTML);
+    flow.compile_to_lowered_stmt("flow.html", {}, HTML);
 
     printf("Success!\n");
     return 0;
