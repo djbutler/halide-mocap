87c294130aeea486fb3fc77a25f232a738415d5d
Dan Butler <djbutler@cs.washington.edu>
2016-04-24 22:42:36 -0700

    first attempt at using gpu_tile

--- src/block_match_flow_generate_arm32.cpp
+++ src/block_match_flow_generate_arm32.cpp
@@ -57,17 +57,22 @@ int main(int argc, char **argv) {
     Var xo, yo, xi, yi;
     Var tile_index;
     flow
+        .gpu_tile(x, y, 16, 16);
+    /*
         .tile(x, y, xo, yo, xi, yi, 128, 128)
         .fuse(xo, yo, tile_index)
         .parallel(tile_index);
 
     flow.vectorize(xi, 4);
+    */
 
     // Store only enough of conv at a time to compute each tile
-    //conv.store_at(flow, xo);
-    conv.compute_at(flow, xi);
+    conv.compute_at(flow, Var::outermost());
+    //conv.gpu_tile(x, y, 16, 16); // Cannot parallelize dimension x.__thread_id_x of function conv because the function is scheduled inline
+//    conv.store_at(flow, xo);
+//    conv.compute_at(flow, xi);
     // Also vectorize the producer (because sin is vectorizable on x86 using SSE).
-    conv.vectorize(i, 4);
+//    conv.vectorize(i, 4);
 
     // Declare the arguments.
     //Param<uint8_t> offset;
